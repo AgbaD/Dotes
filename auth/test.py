bb54d29153d0aa4d3632ce0a9dd1fc3dd17cb90b
Damilare Agba <damilareagba1@gmail.com>
2021-07-25 20:13:35 +0100

    test suite

--- auth/test.py
+++ auth/test.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
 # Author:   @AgbaD || @agba_dr3
 
+from app import app
+import unittest
+
+
+class AuthTest(unittest.TestCase):
+
+    def test_get_login(self):
+        tester = app.test_client(self)
+        response = tester.get("/login")
+        status_code = response.status_code
+        self.assertEqual(status_code, 400)
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
